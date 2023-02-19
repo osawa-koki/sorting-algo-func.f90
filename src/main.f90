4f5392cb1081ddf436efea25f6c530a4ef1821f6
osawa-koki <github@osawa.dev>
2023-02-19 21:35:31 +0900

    小さな修正。

--- src/main.f90
+++ src/main.f90
@@ -2,9 +2,9 @@ program main
     implicit none
     integer, dimension(10) :: numbers = [7, 1, 10, 3, 5, 2, 8, 9, 4, 6]
     integer :: n
-
     n = size(numbers)
+    
     call bubble_sort(numbers(1:n), n)
     write(*,*) 'Bubble Sort: ', numbers
-    
+
 end program main
