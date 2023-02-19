23c82a3aa1599e23c283ad5b7b9d818db86da668
osawa-koki <github@osawa.dev>
2023-02-19 21:48:04 +0900

    小さな修正。

--- src/main.f90
+++ src/main.f90
@@ -2,12 +2,15 @@ program main
     implicit none
     integer, dimension(10) :: numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     integer :: n
+    character(len=15) :: title
     n = size(numbers)
 
+    title = 'Shuffled'
     call shuffle(numbers(1:n), n)
-    write(*,*) 'Shuffled: ', numbers
+    write(*,*) title, ': ', numbers
 
+    title = 'Bubble Sort'
     call bubble_sort(numbers(1:n), n)
-    write(*,*) 'Bubble Sort: ', numbers
+    write(*,*) title, ': ', numbers
 
 end program main
